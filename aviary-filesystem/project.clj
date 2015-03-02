30c590becff4e6ec5311f7c1eb933a1412c24e9f
Will Farrell <will@mojotech.com>
2015-03-02 12:14:18 -0500

    Version 0.1.3-SNAPSHOT

--- aviary-filesystem/project.clj
+++ aviary-filesystem/project.clj
@@ -1,4 +1,4 @@
-(defproject aviary/aviary-filesystem "0.1.2"
+(defproject aviary/aviary-filesystem "0.1.3-SNAPSHOT"
   :description "Aviary module for manipulating the filesystem."
   :dependencies [[org.clojure/clojure _]
                  [aviary/aviary-core _]
