7fb08561cb5099cef7a59fc5573f0e4ffce42893
Will Farrell <will@mojotech.com>
2015-03-02 10:17:59 -0500

    Add SCM info for all projects.

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject aviary/aviary-core "0.1.2-SNAPSHOT"
   :description "Aviary core module."
-  :url "https://github.com/wkf/aviary"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure _]
                  [org.clojure/core.async _]
                  [com.stuartsierra/component _]]
