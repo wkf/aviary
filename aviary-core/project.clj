700598ebad8971dc7db1a4f1217029ff8416e102
Will Farrell <will@mojotech.com>
2015-03-04 23:34:37 -0500

    Version 0.1.7

--- aviary-core/project.clj
+++ aviary-core/project.clj
@@ -1,4 +1,4 @@
-(defproject aviary/aviary-core "0.1.7-SNAPSHOT"
+(defproject aviary/aviary-core "0.1.7"
   :description "Aviary core module."
   :dependencies [[org.clojure/clojure _]
                  [org.clojure/core.async _]
--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,4 +1,4 @@
-(defproject aviary/lein-template "0.1.7-SNAPSHOT"
+(defproject aviary/lein-template "0.1.7"
   :description "A Leiningen template for a basic Aviary project."
   :plugins [[lein-modules "0.3.10"]]
   :eval-in-leiningen true)
