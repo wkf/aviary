f72982e46a3b280cdfb5276983f4fc7bfe49aa47
Will Farrell <will@mojotech.com>
2015-03-05 10:40:31 -0500

    Version 0.1.11

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,4 +1,4 @@
-(defproject aviary/lein-template "0.1.11-SNAPSHOT"
+(defproject aviary/lein-template "0.1.11"
   :description "A Leiningen template for a basic Aviary project."
   :plugins [[lein-modules "0.3.10"]]
   :eval-in-leiningen true)
