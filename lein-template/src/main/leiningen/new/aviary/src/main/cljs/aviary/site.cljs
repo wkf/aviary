ca6024649b04fb6126d9139f21306642c6875654
Will Farrell <will@mojotech.com>
2015-03-05 10:40:02 -0500

    lein-template: Add default cljs file.

---
+++ lein-template/src/main/leiningen/new/aviary/src/main/cljs/aviary/site.cljs
@@ -0,0 +1,17 @@
+(ns {{namespace}}.core)
+
+(defonce site (atom {:running? false}))
+
+(defn start
+  "Start the site. Attempt to be idempotent."
+  []
+  (when-not (:running? @site)
+    (swap! site assoc :running? true)))
+
+(defn stop
+  "Stop the site. Attempt to be idempotent. Useful for interactive local development."
+  []
+  (when (:running? @site)
+    (swap! site assoc :running? false)))
+
+(start)
