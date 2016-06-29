a9e5b3b5c11e68c28841907f918beecf94a3f268
Will Farrell <will@mojotech.com>
2016-06-28 21:55:06 -0400

    Version 0.1.19-SNAPSHOT

--- aviary-www/project.clj
+++ aviary-www/project.clj
@@ -1,4 +1,4 @@
-(defproject aviary/aviary-www "0.1.5"
+(defproject aviary/aviary-www "0.1.6-SNAPSHOT"
   :description "GitHub pages site for Aviary."
   :url "https://github.com/wkf/aviary"
   :scm {:name "git"
