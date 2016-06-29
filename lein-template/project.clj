e2f6fdcf26427eb33bb8d98463b1cdce09181f00
Will Farrell <will@mojotech.com>
2016-06-28 22:00:09 -0400

    Version 0.1.20-SNAPSHOT

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,4 +1,4 @@
-(defproject aviary/lein-template "0.1.19"
+(defproject aviary/lein-template "0.1.20-SNAPSHOT"
   :description "A Leiningen template for a basic Aviary project."
   :url "https://github.com/wkf/aviary"
   :scm {:name "git"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aviary "0.1.19"
+(defproject aviary "0.1.20-SNAPSHOT"
   :url "https://github.com/wkf/aviary"
   :scm {:name "git"
         :url "https://github.com/wkf/aviary"}
